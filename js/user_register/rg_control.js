4ad95cb05c6af26004d21c7a61951efe7499ce56
Felipe Streva <fstreva@poli.ufrj.br>
2020-07-17 05:54:59 -0300

    teste

--- js/user_register/rg_control.js
+++ js/user_register/rg_control.js
@@ -1,5 +1,6 @@
 let rg = document.querySelector('#inputRG')
 rg.addEventListener('keyup', (event)=>{
+    alert(event.key)
     if(!(event.key=='Backspace' || event.key=='Delete')){
         if(event.target.value.length==2 || event.target.value.length==6 ){
             event.target.value += "."
