eec9341b4a89db72fea768f441e58c0b3ecad1d5
Felipe Streva <fstreva@poli.ufrj.br>
2020-07-17 05:50:51 -0300

    fixed bug mobile keycode

--- js/user_register/cep_control.js
+++ js/user_register/cep_control.js
@@ -1,6 +1,6 @@
 let cep = document.querySelector('#inputCEP')
 cep.addEventListener('keyup', (event)=>{
-    if(!(event.code=='Backspace' || event.code=='Delete')){
+    if(!(event.key=='Backspace' || event.key=='Delete')){
         if(event.target.value.length==5){
             event.target.value += "-"
         }
--- js/user_register/rg_control.js
+++ js/user_register/rg_control.js
@@ -1,6 +1,6 @@
 let rg = document.querySelector('#inputRG')
 rg.addEventListener('keyup', (event)=>{
-    if(!(event.code=='Backspace' || event.code=='Delete')){
+    if(!(event.key=='Backspace' || event.key=='Delete')){
         if(event.target.value.length==2 || event.target.value.length==6 ){
             event.target.value += "."
         } else if (event.target.value.length== 10){
