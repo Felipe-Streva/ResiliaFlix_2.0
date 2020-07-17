655ea2aebcca4084d7ab8fd1b09116dc4ff6b6d9
Felipe Streva <fstreva@poli.ufrj.br>
2020-07-16 22:34:22 -0300

    add backspace mobile

--- js/user_register/rg_control.js
+++ js/user_register/rg_control.js
@@ -1,7 +1,7 @@
 let rg = document.querySelector('#inputRG')
 rg.addEventListener('keyup', (event)=>{
-    if(!(event.keyCode==8 || event.keyCode==46)){
-        if(event.target.value.length==2 || event.target.value.length==6){
+    if(!(event.keyCode==8 || event.keyCode==46 || event.keyCode==229)){
+        if(event.target.value.length==2 || event.target.value.length==6 ){
             event.target.value += "."
         } else if (event.target.value.length== 10){
             event.target.value += "-"
