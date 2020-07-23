6107e4620fffcaab29b9d742eb3401be228f35c2
Felipe Streva <fstreva@poli.ufrj.br>
2020-07-23 16:23:58 -0300

    add hover

--- js/search_page/SearchViews.js
+++ js/search_page/SearchViews.js
@@ -1,6 +1,6 @@
 class SearchViews{
 
     static template(infos){
-        return `<img src="${infos.poster}" class="img-thumbnail col-12 col-md-2">`
+        return `<img src="${infos.poster}" class="img-thumbnail col-12 col-md-3 shadow p-3 mb-5 bg-white rounded movie-image">`
     }
 }
