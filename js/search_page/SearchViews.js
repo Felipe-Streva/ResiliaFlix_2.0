08a97288ae90186be2445bc12356bf40e03a2662
Felipe Streva <fstreva@poli.ufrj.br>
2020-07-24 21:16:35 -0300

    grid search

--- js/search_page/SearchViews.js
+++ js/search_page/SearchViews.js
@@ -1,6 +1,6 @@
 class SearchViews{
 
     static template(infos){
-        return `<img src="${infos.poster}" class="img-thumbnail col-12 col-md-3 shadow p-2 mb-5 bg-white rounded movie-image" onclick="ModalController.newInfos('${infos.imdbID}')" data-toggle="modal" data-target="#ModalScrollable">`
+        return `<img src="${infos.poster}" class="img-thumbnail col-12 col-sm-6 col-md-4 col-lg-3 shadow p-2 mb-5 bg-white rounded movie-image" onclick="ModalController.newInfos('${infos.imdbID}')" data-toggle="modal" data-target="#ModalScrollable">`
     }
 }
